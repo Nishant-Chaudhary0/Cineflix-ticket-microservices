4cc9c2f7ef16395f5582fbf45700d43569692001
Nishant Chaudhary <156567884+Nishant-Chaudhary0@users.noreply.github.com>
2026-08-24 17:39:11 +0530

    Delete src directory

--- src/routes/theatreRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createTheatre, getAllTheatres } from "../controllers/theatrecontroller.js";
-
-const theatreRouter = express.Router();
-
-theatreRouter.post("/create-theatre", createTheatre);
-theatreRouter.get("/get-all-theatres", getAllTheatres);
-
-export default theatreRouter;
